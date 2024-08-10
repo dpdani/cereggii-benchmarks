69d26c14a90ed16876fe008d903bff6960741118
Daniele Parmeggiani <8658291+dpdani@users.noreply.github.com>
2024-08-10 10:28:29 +0200

    Update install-and-run.sh

--- install-and-run.sh
+++ install-and-run.sh
@@ -7,7 +7,7 @@ cd cereggii-benchmarks
 ./enable-perf.sh
 git submodule update --init
 cd growt
-git submodule update utils
+git submodule update --init utils
 cd ..
 docker build --tag 'cereggii_benchmarks' .
 mkdir -p ./reports
